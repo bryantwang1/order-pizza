e1b0240b03ebd322bd3b1aaddfad0f2159ae81cd
Bryant Wang <bryant.m.wang@gmail.com>
2016-11-04 09:43:32 -0700

    Add pricer prototype method to Pizza objects that generates a price depending on its properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,45 @@
+var priceyToppings = ["tuna", "prosciutto", "shrimp"];
+
+
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
+  this.price = 8;
+}
+
+Pizza.prototype.pricer = function() {
+  console.log("pricer run");
+  this.price += ((this.size - 8) / 2);
+
+  for(var idx = 0; idx < this.toppings.length ;idx++) {
+    console.log("enter loop");
+    if(priceyToppings.includes(this.toppings[idx])) {
+      this.price += 2;
+    } else {
+      this.price += 1;
+    }
+  }
 }
 
 // UI below this line.
 
 $(function() {
 
-  $("#new-pizza1").submit(function(event) {
-    event.preventDefault;
+  $("form#new-pizza1").submit(function(event) {
+    event.preventDefault();
 
     var pizzaSize = parseInt($("#pizza1-size").val());
 
     var pizza1 = new Pizza(pizzaSize);
+    console.log(pizza1.price);
 
     $("input:checkbox[name=pizza1-toppings]:checked").each(function(){
     pizza1.toppings.push($(this).val());
     });
 
-    alert("You chose a " + pizza1.size + "\" pizza with " + pizza1.toppings + "!");
+    pizza1.pricer();
+
+    alert("You chose a " + pizza1.size + "\" pizza with " + pizza1.toppings + "! Your price is: $" + pizza1.price);
   });
 
 
