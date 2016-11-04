6b47906af7122884c6cfaa9cae0d9423164da2cf
Bryant Wang <bryant.m.wang@gmail.com>
2016-11-04 09:14:00 -0700

    Add alert to confirm that pizza toppings data has been taken.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 $(function() {
+  var pizza1Proteins = [];
+  var pizza1Veggies = [];
 
   $("#new-pizza1").submit(function(event) {
+    event.preventDefault;
+
     var pizzaSize = parseInt($("#pizza1-size").val());
-    alert("You chose a " + pizzaSize + "\" pizza!");
+
+    $("input:checkbox[name=pizza1-protein]:checked").each(function(){
+    pizza1Proteins.push($(this).val());
+    });
+
+    $("input:checkbox[name=pizza1-veggie]:checked").each(function(){
+    pizza1Veggies.push($(this).val());
+    });
+
+    alert("You chose a " + pizzaSize + "\" pizza with " + pizza1Proteins + " and " + pizza1Veggies + "!");
   });
 
 
